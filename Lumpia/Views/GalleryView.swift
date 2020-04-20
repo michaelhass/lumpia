11b43792de2abb27db19b405942f38f8b21796c2
Michel Hass <4216942+michaelshass@users.noreply.github.com>
2020-04-20 23:11:33 +0900

    Implement SquareImage

--- Lumpia/Views/GalleryView.swift
+++ Lumpia/Views/GalleryView.swift
@@ -28,26 +28,6 @@ struct GalleryView: View {
         .padding(.horizontal)
         .background(Color.white)
 
-
-    }
-}
-
-struct SquaredImage: View {
-
-    let width: CGFloat
-
-    init(width: CGFloat) {
-        self.width = width
-    }
-
-    var body: some View {
-        ZStack(alignment: .center) {
-            Image("test_image")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: width, height: width, alignment: .center)
-                .clipped()
-        }
     }
 }
 
